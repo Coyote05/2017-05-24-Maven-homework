5be3e6bdd70a7eb12f45433f2272a48e6e77f9f8
Norman Teibert <norman.teibert@gmail.com>
2017-05-28 00:50:36 +0200

    #2

---
+++ src/main/java/homework11/Data.java
@@ -0,0 +1,52 @@
+package homework11;
+
+import java.util.List;
+
+public class Data {
+
+    private String updated;
+    private Integer totalItems;
+    private Integer startIndex;
+    private Integer itemsPerPage;
+    private List<Items> items;
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public Integer getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(Integer totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public Integer getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(Integer startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public Integer getItemsPerPage() {
+        return itemsPerPage;
+    }
+
+    public void setItemsPerPage(Integer itemsPerPage) {
+        this.itemsPerPage = itemsPerPage;
+    }
+
+    public List<Items> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Items> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/homework11/Youtube.java
@@ -0,0 +1,23 @@
+package homework11;
+
+public class Youtube {
+
+    private Double apiVersion;
+    private Data data;
+
+    public Double getApiVersion() {
+        return apiVersion;
+    }
+
+    public void setApiVersion(Double apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+}
