05ac4236755a2b2067d3d7d34503bc11116f899c
Norman Teibert <norman.teibert@gmail.com>
2017-05-28 22:06:08 +0200

    #3

--- src/main/java/homework11/Util.java
+++ src/main/java/homework11/Util.java
@@ -11,15 +11,20 @@ import java.nio.file.Paths;
 
 public class Util {
 
-    public static void printYoutubeJSON (String place) throws Exception{
+    public static void printYoutubeJSON (String place) {
 
-        byte[] jsonData = Files.readAllBytes(Paths.get(place));
+        try {
+            byte[] jsonData = Files.readAllBytes(Paths.get(place));
 
-        ObjectMapper objectMapper = new ObjectMapper();
+            ObjectMapper objectMapper = new ObjectMapper();
 
-        Youtube youtube = objectMapper.readValue(jsonData, Youtube.class);
+            Youtube youtube = objectMapper.readValue(jsonData, Youtube.class);
 
-        System.out.println(objectMapper.writeValueAsString(youtube));
+            System.out.println(objectMapper.writeValueAsString(youtube));
+
+        } catch (IOException e) {
+            System.out.println("IOException " + e.getMessage());
+        }
     }
 
     public static void convertYoutubeJSONToXML (String fromPlace, String toPlace) throws IOException, XMLStreamException {
